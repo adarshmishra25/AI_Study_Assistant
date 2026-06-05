60277f0d6e5f16ca4e5487507fd26d660b7a4993
Adarsh Mishra <adarshmishra.2507@gmail.com>
2026-06-05 13:16:47 +0530

    Day 1 setup

---
+++ ai-study-assitant/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>AI Learning Assistant</h1>
+      <p>Upload notes, generate summaries, key points, quizzes, and ask questions.</p>
+
+      <button>Upload PDF</button>
+      <button>Generate Summary</button>
+      <button>Generate Key Points</button>
+      <button>Generate Quiz</button>
+      <button>Ask Questions</button>
+    </div>
+  );
+}
+
+export default App;
