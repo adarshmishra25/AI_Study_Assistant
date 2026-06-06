d525122a44775c9975d4fb57841bbe6ab9a0e49e
Adarsh Mishra <adarshmishra.2507@gmail.com>
2026-06-06 15:44:08 +0530

    Showing uploaded document

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
   return (
     <div className="main">
       <div className="heading">
         <h1>AI Learning Assistant</h1>
-      <p>
-        Upload notes, generate summaries, key points, quizzes, and ask
-        questions.
-      </p>
+        <p>
+          Upload notes, generate summaries, key points, quizzes, and ask
+          questions.
+        </p>
       </div>
 
 
-      
-      <button>Upload PDF</button>
+
+
+      <input type="file" accept=".pdf" onChange={(event) => { setSelectedFile(event.target.files[0]) }} />
 
       <div className="documents">
         <h2>Uploaded Documents</h2>
-        <p>No PDF uploaded yet.</p>
+        {selectedFile ? (
+          <p>{selectedFile.name}</p>
+        ) : (
+          <p>Upload a PDF to get started.</p>
+        )}
       </div>
 
-      
+
       <div className="action">
         <h1>Actions</h1>
         <button>Generate Summary</button>
@@ -28,8 +37,8 @@ function App() {
         <button>Generate Quiz</button>
         <button>Ask Questions</button>
       </div>
-      
-      
+
+
 
     </div>
   );
