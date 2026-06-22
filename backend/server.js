4a1da5220e73631de98507a3f6235ff1ec8af09a
Adarsh Mishra <adarshmishra.2507@gmail.com>
2026-06-22 14:17:13 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,8 @@ async function testPrisma() {
 
 testPrisma();
 
-app.listen(5000, () => { console.log("Server running on port 5000")});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
